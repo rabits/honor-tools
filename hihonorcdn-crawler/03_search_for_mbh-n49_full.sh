e42253a14dc90c9b52ccb69d85bca773c089fd3d
Rabit <home@rabits.org>
2026-09-15 12:36:06 -0400

    Added Magic V6 as well

--- hihonorcdn-crawler/03_search_for_mbh-n49_full.sh
+++ hihonorcdn-crawler/03_search_for_mbh-n49_full.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+# Honor Magic V5 - MBH-N49
 
 SEARCH_DIR="$1"
-[ "x$SEARCH_DIR" != 'x' ] || exit 1
+if [ "x$SEARCH_DIR" = 'x' ]; then
+    echo 'Please specify directory to search in'
+    exit 1
+fi
 
 grep -r 'MBH-LGRP2-OVS' "$SEARCH_DIR" | grep 'version_mbn' | sed 's/\(.*\):\(.*\)/\2        \1/' | sort
---
+++ hihonorcdn-crawler/04_search_for_pnm-n49_cust_pre.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Honor Magic V6 - PNM-N49
+
+SEARCH_DIR="$1"
+if [ "x$SEARCH_DIR" = 'x' ]; then
+    echo 'Please specify directory to search in'
+    exit 1
+fi
+
+grep -r 'PNM-N49-' "$SEARCH_DIR" | grep 'version_mbn' | sed 's/\(.*\):\(.*\)/\2        \1/' | sort
